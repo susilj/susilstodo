e4a3275e46fdb803fb26da30e9330b655f6a5e53
susilj <susil_j@yahoo.co.in>
2018-01-29 20:19:05 +0530

    removed AWS details

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,8 +1,8 @@
 const config = {
-    AWS_ACCESS_KEY_ID: 'AKIAIVSGIB4OQ32MPIEA',
-    AWS_SECRET_ACCESS_KEY: 'I0RTRMtz5QnnRhCRZcV1x4jv+Iq60fCcQ7hicQnd',
-    HOST: 'smrvj2g4zfcblctfarrtvlreze.appsync-api.us-east-1.amazonaws.com', // Your hostname
-    REGION: 'us-east-1',  //Your region
+    AWS_ACCESS_KEY_ID: '',
+    AWS_SECRET_ACCESS_KEY: '',
+    HOST: '', // Your hostname
+    REGION: '',  //Your region
     PATH: '/graphql',
 }
 config.ENDPOINT = `https://${config.HOST}${config.PATH}`;
